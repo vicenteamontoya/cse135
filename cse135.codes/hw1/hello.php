8b544df6e5d59a227e35a3c562a889a382c6cb07
vicenteamontoya <vicenteamontoya@gmail.com>
2022-04-13 17:08:46 -0700

    favicon on php page

--- cse135.codes/hw1/hello.php
+++ cse135.codes/hw1/hello.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Hello PHP</title>
+    <link rel="icon" type="image/x-icon" href="..//favicon.ico">
 </head>
 <body>
     <h1>PHP Lives!</h1>
